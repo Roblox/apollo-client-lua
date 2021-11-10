ad24802d5dadbf2f134b56a9d7603852c003ba07
Anonymous Author <roflection-bot@roblox.com>
2021-11-10 07:28:31 -0300

    align apollo provider tests with upstream (#249)

--- src/react/context/__tests__/ApolloProvider.spec.lua
+++ src/react/context/__tests__/ApolloProvider.spec.lua
@@ -1,105 +1,120 @@
 -- ROBLOX upstream: https://github.com/apollographql/apollo-client/blob/v3.4.0-rc.6/src/react/context/__tests__/ApolloProvider.test.tsx
---!nocheck
 
 return function()
-	local rootWorkspace = script.Parent.Parent.Parent.Parent
-	local packagesWorkspace = rootWorkspace.Parent.Parent.Packages
+	local srcWorkspace = script.Parent.Parent.Parent.Parent
+	local rootWorkspace = srcWorkspace.Parent.Parent.Packages
 
-	local JestGlobals = require(packagesWorkspace.Dev.JestGlobals)
+	local JestGlobals = require(rootWorkspace.Dev.JestGlobals)
 	local jestExpect = JestGlobals.expect
 
-	local bootstrap = require(rootWorkspace.utilities.common.bootstrap)
+	local LuauPolyfill = require(rootWorkspace.LuauPolyfill)
+	local Boolean = LuauPolyfill.Boolean
+	local console = LuauPolyfill.console
 
-	local LuauPolyfill = require(packagesWorkspace.LuauPolyfill)
-	local Array = LuauPolyfill.Array
-
-	local React = require(packagesWorkspace.React)
+	local React = require(rootWorkspace.React)
 	local useContext = React.useContext
 
-	local ApolloClient = require(rootWorkspace.core).ApolloClient
+	local testingLibraryModule = require(srcWorkspace.testUtils.react)
+	local render = testingLibraryModule.render
+	local cleanup = testingLibraryModule.cleanup
+
+	local ApolloLink = require(srcWorkspace.link.core).ApolloLink
+	local apolloClientModule = require(srcWorkspace.core.ApolloClient)
+	local ApolloClient = apolloClientModule.ApolloClient
+	type ApolloClient<TCacheShape> = apolloClientModule.ApolloClient<TCacheShape>
+	local Cache = require(srcWorkspace.cache.inmemory.inMemoryCache).InMemoryCache
 
 	local ApolloContextModule = require(script.Parent.Parent)
 	local ApolloProvider = ApolloContextModule.ApolloProvider
 	local getApolloContext = ApolloContextModule.getApolloContext
 
 	describe("<ApolloProvider /> Component", function()
-		local client = ApolloClient.new({ cache = nil })
-		local rootInstance
-		local stop: (() -> ())?
-		local TestTextLabelElement = React.createElement("TextLabel", { Text = "Test" })
+		afterEach(cleanup)
 
-		beforeEach(function()
-			rootInstance = Instance.new("Folder")
-			rootInstance.Name = "GuiRoot"
-		end)
-
-		afterEach(function()
-			if typeof(stop) == "function" then
-				stop()
-			end
-		end)
+		local client = ApolloClient.new({
+			cache = Cache.new(),
+			link = ApolloLink.new(function(_self, o, f)
+				if Boolean.toJSBoolean(f) then
+					return f(o)
+				else
+					return nil
+				end
+			end),
+		})
 
 		it("should render children components", function()
-			local testApolloProvider = function()
-				return React.createElement(ApolloProvider, { client = client }, TestTextLabelElement)
-			end
-			stop = bootstrap(rootInstance, testApolloProvider)
-			local descendants = rootInstance:GetDescendants()
-			local count = #Array.filter(descendants, function(item)
-				return item.Name == "TextLabel"
-			end, nil)
-			jestExpect(count).toBe(1)
+			local ref = render(
+				React.createElement(
+					ApolloProvider,
+					{ client = client },
+					React.createElement("TextLabel", { Text = "Test" })
+				)
+			)
+			jestExpect(ref.getByText("Test")).toBeTruthy()
 		end)
 
 		it("should support the 2.0", function()
-			local testApolloProvider = function()
-				return React.createElement(ApolloProvider, { client = {} }, TestTextLabelElement)
-			end
-			stop = bootstrap(rootInstance, testApolloProvider)
-			local descendants = rootInstance:GetDescendants()
-			local count = #Array.filter(descendants, function(item)
-				return item.Name == "TextLabel"
-			end, nil)
-			jestExpect(count).toBe(1)
+			local ref = render(
+				React.createElement(
+					ApolloProvider,
+					{ client = {} :: ApolloClient<any> },
+					React.createElement("TextLabel", { Text = "Test" })
+				)
+			)
+			jestExpect(ref.getByText("Test")).toBeTruthy()
 		end)
 
 		it("should require a client", function()
-			local ApolloContext = getApolloContext()
+			local originalConsoleError = console.error
+			console.error = function()
+				--[[ noop ]]
+			end
 			jestExpect(function()
-				local resetApolloContext = function()
-					return React.createElement(ApolloContext.Provider, { value = {} }, {
-						React.createElement(ApolloProvider, { client = nil }, TestTextLabelElement),
-					})
-				end
-				stop = bootstrap(rootInstance, resetApolloContext)
-			end).toThrow(
+				-- Before testing `ApolloProvider`, we first fully reset the
+				-- existing context using `ApolloContext.Provider` directly.
+				local ApolloContext = getApolloContext()
+				render(
+					React.createElement(
+						ApolloContext.Provider,
+						{ value = {} },
+						React.createElement(
+							ApolloProvider,
+							{ client = nil :: any },
+							React.createElement("TextLabel", { Text = "" })
+						)
+					)
+				)
+			end).toThrowError(
 				"ApolloProvider was not passed a client instance. Make "
 					.. 'sure you pass in your client via the "client" prop.'
 			)
+			console.error = originalConsoleError
 		end)
 
 		it("should not require a store", function()
-			local testApolloProvider = function()
-				return React.createElement(ApolloProvider, { client = client }, TestTextLabelElement)
-			end
-			stop = bootstrap(rootInstance, testApolloProvider)
-			local descendants = rootInstance:GetDescendants()
-			local count = #Array.filter(descendants, function(item)
-				return item.Name == "TextLabel"
-			end, nil)
-			jestExpect(count).toBe(1)
+			local ref = render(
+				React.createElement(
+					ApolloProvider,
+					{ client = client },
+					React.createElement("TextLabel", { Text = "Test" })
+				)
+			)
+			jestExpect(ref.getByText("Test")).toBeTruthy()
 		end)
 
 		it("should add the client to the children context", function()
 			local TestChild = function()
 				local context = useContext(getApolloContext(), nil, nil)
-				jestExpect(context.client).toBe(client)
+				jestExpect(context.client).toEqual(client)
 				return nil
 			end
-			local testApolloProvider = function()
-				return React.createElement(ApolloProvider, { client = client }, React.createElement(TestChild))
-			end
-			stop = bootstrap(rootInstance, testApolloProvider)
+			render(
+				React.createElement(
+					ApolloProvider,
+					{ client = client },
+					{ React.createElement(TestChild), React.createElement(TestChild) }
+				)
+			)
 		end)
 
 		it("should update props when the client changes", function()
@@ -109,13 +124,23 @@ return function()
 				jestExpect(context.client).toBe(clientToCheck)
 				return nil
 			end
-			local testApolloProvider = function()
-				return React.createElement(ApolloProvider, { client = clientToCheck }, React.createElement(TestChild))
-			end
-			stop = bootstrap(rootInstance, testApolloProvider)
-			clientToCheck = ApolloClient.new({ cache = nil })
-			local stopRerender = bootstrap(rootInstance, testApolloProvider)
-			stopRerender()
+			local ref = render(
+				React.createElement(ApolloProvider, { client = clientToCheck }, React.createElement(TestChild))
+			)
+			local newClient = ApolloClient.new({
+				cache = Cache.new(),
+				link = ApolloLink.new(function(_self, o, f)
+					if Boolean.toJSBoolean(f) then
+						return f(o)
+					else
+						return nil
+					end
+				end),
+			})
+			clientToCheck = newClient
+			ref.rerender(
+				React.createElement(ApolloProvider, { client = clientToCheck }, React.createElement(TestChild))
+			)
 		end)
 	end)
 end
