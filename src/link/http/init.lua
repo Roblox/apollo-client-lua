76c7773d509bdddcd9c6b37c195b475826f4a8c4
Anonymous Author <roflection-bot@roblox.com>
2021-11-24 19:44:33 +0100

    enable possible tests (#268)

--- src/link/http/init.lua
+++ src/link/http/init.lua
@@ -2,21 +2,21 @@
 
 local exports = {}
 
--- local parseAndCheckHttpResponseModule = require(script.parseAndCheckHttpResponse)
--- exports.parseAndCheckHttpResponse = parseAndCheckHttpResponseModule.parseAndCheckHttpResponse
--- export type ServerParseError = parseAndCheckHttpResponseModule.ServerParseError
+local parseAndCheckHttpResponseModule = require(script.parseAndCheckHttpResponse)
+exports.parseAndCheckHttpResponse = parseAndCheckHttpResponseModule.parseAndCheckHttpResponse
+export type ServerParseError = parseAndCheckHttpResponseModule.ServerParseError
 local serializeFetchParameterModule = require(script.serializeFetchParameter)
 exports.serializeFetchParameter = serializeFetchParameterModule.serializeFetchParameter
 export type ClientParseError = serializeFetchParameterModule.ClientParseError
 local selectHttpOptionsAndBodyModule = require(script.selectHttpOptionsAndBody)
--- export type HttpOptions = selectHttpOptionsAndBodyModule.HttpOptions
--- exports.fallbackHttpConfig = selectHttpOptionsAndBodyModule.fallbackHttpConfig
--- exports.selectHttpOptionsAndBody = selectHttpOptionsAndBodyModule.selectHttpOptionsAndBody
+export type HttpOptions = selectHttpOptionsAndBodyModule.HttpOptions
+exports.fallbackHttpConfig = selectHttpOptionsAndBodyModule.fallbackHttpConfig
+exports.selectHttpOptionsAndBody = selectHttpOptionsAndBodyModule.selectHttpOptionsAndBody
 export type UriFunction = selectHttpOptionsAndBodyModule.UriFunction
 exports.checkFetcher = require(script.checkFetcher).checkFetcher
 exports.createSignalIfSupported = require(script.createSignalIfSupported).createSignalIfSupported
--- exports.selectURI = require(script.selectURI).selectURI
--- exports.createHttpLink = require(script.createHttpLink).createHttpLink
+exports.selectURI = require(script.selectURI).selectURI
+exports.createHttpLink = require(script.createHttpLink).createHttpLink
 local httpLinkModule = require(script.HttpLink)
 exports.HttpLink = httpLinkModule.HttpLink
 export type HttpLink = httpLinkModule.HttpLink
