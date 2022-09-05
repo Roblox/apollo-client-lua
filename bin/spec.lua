cf25f972062768101919908bff1d94bb6f16d55b
Anonymous Author <roflection-bot@roblox.com>
2022-09-05 10:41:05 -0300

    fix spec file (#335)

--- bin/spec.lua
+++ bin/spec.lua
@@ -4,6 +4,10 @@ local Packages = Root.Packages
 
 local runCLI = require(Packages.Dev.Jest).runCLI
 
+local processServiceExists, ProcessService = pcall(function()
+	return game:GetService("ProcessService")
+end)
+
 local status, result = runCLI(Root, {
 	verbose = _G.verbose == "true",
 	ci = _G.CI == "true",
@@ -12,6 +16,16 @@ local status, result = runCLI(Root, {
 
 if status == "Rejected" then
 	print(result)
+end
+
+if status == "Resolved" and result.results.numFailedTestSuites == 0 and result.results.numFailedTests == 0 then
+	if processServiceExists then
+		ProcessService:ExitAsync(0)
+	end
+end
+
+if processServiceExists then
+	ProcessService:ExitAsync(1)
 end
 
 return nil
